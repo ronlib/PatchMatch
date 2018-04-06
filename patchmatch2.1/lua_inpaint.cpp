253c1266facef84f070a0124fc3ba8a55b537450
Ron <ronll64@gmail.com>
2018-04-06 15:41:32 +0300

    Made some progress

--- patchmatch2.1/lua_inpaint.cpp
+++ patchmatch2.1/lua_inpaint.cpp
@@ -34,6 +34,13 @@ static int patchmatch(lua_State *L)
 		}
 
 	Params *p = new Params();
+	BITMAP *a = NULL, *b = NULL, *ann_prev = NULL, *ann_window = NULL, *awinsize = NULL;
+  double *win_size = NULL;
+  BITMAP *amask = NULL, *bmask = NULL;
+  double scalemin = 0.5, scalemax = 2.0;  // The product of these must be one.
+	int sim_mode = 0;
+	int knn_chosen = -1;
+	int enrich_mode = 0;
 	const char * image_file_path = luaL_checkstring(L, i);	i++;
 	const char * mask_file_path = luaL_checkstring(L, i);	i++;
 
@@ -42,7 +49,7 @@ static int patchmatch(lua_State *L)
 
 	if (nin >= i && luaL_checkstring(L, i))
 		{
-			// TODO: add more algorithms
+			// TODO: add more algorithms: gpucpu, cputiled, rotscale, enrich
 			p->algo = ALGO_CPU;
 		} i++;
 
@@ -53,6 +60,39 @@ static int patchmatch(lua_State *L)
 	if (nin >= i && luaL_checknumber(L, i)) {p->rs_ratio = (int)luaL_checknumber(L, i);} i++;
 	if (nin >= i && luaL_checknumber(L, i)) {p->rs_iters = (int)luaL_checknumber(L, i);} i++;
 	if (nin >= i && luaL_checknumber(L, i)) {p->cores = (int)luaL_checknumber(L, i);} i++;
+	if (nin >= i+1 && luaL_checknumber(L, i)) {
+		p->window_w = (int)luaL_checknumber(L, i); i++;
+		p->window_h = (int)luaL_checknumber(L, i);} i++;
+
+	// TODO: complete ann_prev
+	if (nin >= i && lua_isnil(L, i)) {} else {error(L, "Cannot currently handle this parameter");} i++;
+
+	// TODO: complete ann_window
+	if (nin >= i && lua_isnil(L, i)) {} else {error(L, "Cannot currently handle this parameter");} i++;
+
+	// TODO: complete awinsize
+	if (nin >= i && lua_isnil(L, i)) {} else {error(L, "Cannot currently handle this parameter");} i++;
+
+	if (nin >= i && luaL_checknumber(L, i)) {
+		knn_chosen = (int)luaL_checknumber(L, i);
+		if (knn_chosen == 1) { knn_chosen = -1; }
+    if (knn_chosen <= 0) { error(L, "knn is less than zero"); }
+	} i++;
+
+	if (nin >= i && luaL_checknumber(L, i)) {
+		scalemax = luaL_checknumber(L, i);
+		if (scalemax <= 0) { error(L, "\nscalerange is less than zero"); }
+    scalemin = 1.0/scalemax;
+    if (scalemax < scalemin) {
+      double temp = scalemax;
+      scalemax = scalemin;
+      scalemin = temp;
+    }
+	} i++;
+
+	if (ann_window&&!awinsize&&!win_size) {
+    error(L, "\nUsing ann_window - either awinsize or win_size should be defined.\n");
+  }
 }
 
 
