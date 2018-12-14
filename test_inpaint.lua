889c7128cf383b62db89338e93f0d573b10f8443
Ron <ronll64@gmail.com>
2018-12-14 20:08:42 +0200

    Changed defaults in test_inpaint.cpp

--- test_inpaint.lua
+++ test_inpaint.lua
@@ -9,8 +9,8 @@ patch2vec_init('model_750_50000_32_180804.t7')
 nnluainpaint = require("libpatchmatch2")
 nnluainpaint.inpaint('/home/ron/studies/project/PatchMatch/image_files/forest/forest.bmp',
                      '/home/ron/studies/project/PatchMatch/forest-mask-50-points.bmp',
-                     5,         -- nn_iters
-                     6,         -- inpaint_border
-                     0,         -- inpaint_min_level
-                     50         -- threshold
+   7,         -- nn_iters
+   8,         -- inpaint_border
+   0,         -- inpaint_min_level
+   50         -- threshold
 )
